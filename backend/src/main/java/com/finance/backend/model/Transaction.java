911c6d781c9c0a75cdade3dec93ee2eebec4973f
MarcinTyszka <marcin.tyszka2022@gmail.com>
2026-03-01 20:50:19 +0100

    Add transcation model and H2 database configuration

---
+++ backend/src/main/java/com/finance/backend/model/Transaction.java
@@ -0,0 +1,44 @@
+package main.java.com.finance.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+
+@Entity
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+    private String type;
+    private String category;
+    private String description;
+    private LocalDate date;
+
+    public Transaction(){
+
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public BigDecimal getAmount() { return amount; }
+    public void setAmount(BigDecimal amount) { this.amount = amount; }
+
+    public String getType() { return type; }
+    public void setType(String type) { this.type = type; }
+
+    public String getCategory() { return category; }
+    public void setCategory(String category) { this.category = category; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
+}
